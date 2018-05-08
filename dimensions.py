da3490a544f80a7cb77a07ab89347a130fdeb388
suzinicco <lgc@aprogen.com>
2018-05-09 07:57:28 +0900

    큐브연습문제

--- dimensions.py
+++ dimensions.py
@@ -1,7 +1,19 @@
-dimensions = (200,50)
+dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
 
-dimensions2 = (200,50)
+dimensions2 = (200, 50)
 for di in dimensions2:
-	print(di)
+    print(di)
+
+
+menus = ('삼계탕','닭도리탕','냉면','돼지갈비','불고기')
+
+print('안녕하세요 주문하시겠어요? 메뉴는 다음과 같습니다\n')
+for choise in menus:
+    print(choise)
+
+print('재료가 떨어져서 메뉴는 다음과 같습니다')
+menus = ('팥빙수','아메리카노','레몬에이드','카페모카','녹차')
+for choise in menus:
+    print(choise)
