c72d4e5b281ec2f68c307b6e4fc0468d9e543167
suzinicco <lgc@aprogen.com>
2018-05-21 08:35:53 +0900

    2

--- toppings.py
+++ toppings.py
@@ -1,3 +1,11 @@
 requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
+
+
+banned_users = ['andrew','carolina','david']
+user = 'marie'
+
+if user not in banned_users:
+    print(user.title() + ", you can post a response if you wish")
+    
