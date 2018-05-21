5c0feac5eb0d0776b8179b0dea5c12bd98325123
suzinicco <lgc@aprogen.com>
2018-05-21 14:04:51 +0900

    20180521-3

--- toppings.py
+++ toppings.py
@@ -8,4 +8,15 @@ user = 'marie'
 
 if user not in banned_users:
     print(user.title() + ", you can post a response if you wish")
-    
+
+
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+
+print("Your admission cost is $" + str(price) + ".")
