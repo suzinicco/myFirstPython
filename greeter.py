8660c4aad3ab5fa5aa6034c6bf8db3ffc8c9668b
suzinicco <lgc@aprogen.com>
2018-06-08 08:25:15 +0900

    함수를 배웠다

---
+++ greeter.py
@@ -0,0 +1,29 @@
+def greet_user():
+    """Summary
+    """
+    print("Hello!")
+
+
+
+
+
+def bye_user():
+    """Summary
+    """
+    print("Bye!")
+
+def add_user():
+    """Summary
+    """
+    #이것도 되는건가
+    print("Welcome")
+
+
+greet_user()
+
+
+bye_user()
+
+add_user()
+
+
